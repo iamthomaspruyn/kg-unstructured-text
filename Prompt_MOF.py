cdf34ebbff81ed90c4b76fccbc8ed249cbea25ea
iamthomaspruyn <tompruyn84@gmail.com>
2024-10-01 16:42:21 -0400

    added prompts incorperating metadata

---
+++ Prompt_MOF.py
@@ -0,0 +1,190 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+examples = [
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)SO 4]·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Metal",
+            "tail": "copper(II)",
+            "tail_type": "Metal"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)SO 4]·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Linker",
+            "tail": "1,3,5-tris(1-imidazolyl)benzene",
+            "tail_type": "Linker"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)SO 4]·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Alias",
+            "tail": "Cu1",
+            "tail_type": "Alias"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](CH3COO)2·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Metal",
+            "tail": "copper(II)",
+            "tail_type": "Metal"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](CH3COO)2·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Linker",
+            "tail": "1,3,5-tris(1-imidazolyl)benzene",
+            "tail_type": "Linker"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](CH3COO)2·H2O",
+            "head_type": "MOF",
+            "relation": "Has_Alias",
+            "tail": "Cu2",
+            "tail_type": "Alias"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](ClO4)2",
+            "head_type": "MOF",
+            "relation": "Has_Metal",
+            "tail": "copper(II)",
+            "tail_type": "Metal"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](ClO4)2",
+            "head_type": "MOF",
+            "relation": "Has_Linker",
+            "tail": "1,3,5-tris(1-imidazolyl)benzene",
+            "tail_type": "Linker"
+        },
+        {
+            "text": ("Here, three Cu-MOFs with diﬀerent copper(II) site distribution were employed for CO2 electroreduction. The Cu-MOFs [Cu(L)SO 4]·H2O (Cu1), "
+                    "[Cu(L)2(H2O)2](CH3COO)2·H2O (Cu2), and [Cu(L)2(H2O)2](ClO4)2 (Cu3) were achieved by using the same ligand 1,3,5-tris(1-imidazolyl)benzene (L) but different Cu(II) salts."),
+            "head": "[Cu(L)2(H2O)2](ClO4)2",
+            "head_type": "MOF",
+            "relation": "Has_Alias",
+            "tail": "Cu3",
+            "tail_type": "Alias"
+        }
+]
+
+
+
+context = ''' 
+
+Here is some context to help you perform this task, and identify the nodes and relationships:
+
+### **Node Types:**
+- **MOF (Metal-Organic Framework):** Refers to compounds consisting of metal ions or clusters coordinated to organic ligands. MOFs are sometimes reffered to as "Coordination Polymers". No standard naming convention exists, and they may or may not be presented as a chemical formula. Only create nodes for specific MOFs named in the paper! If the paper mentions "Zinc MOFs", do not extract this as a node. But if the paper said "[Zn₄O(BDC)₃]", do extract this as a node.
+
+### **Relationship Types:**
+- **Has_Alias:** Indicates that two "MOF" type nodes refer to the same MOF, just under a different name. Ex: "MOF-801" may be reffered to as "Complex 1" in a different part of the text. 
+
+### **Important Guidelines:**
+1) No standard naming convention exists for MOFs. It is your job to correctly identify MOF node types in the given text. Here are some examples of how MOFs can be named:
+    i) Based on institution of research group - Ex: "HKUST-1"
+    ii) Based on order of discovery - Ex: "MOF-5"
+    iii) Based on chemical formulas - Ex: "[Zr₆O₄(OH)₄(BDC)₆]"
+
+2) It is your job to identify when the author of the text referring to a previously mentioned MOF under different naming convention. Give a "has_alias" relationship to these MOFs if you believe with certaintity that this is the case.
+    i) Ex: "HKUST-1", "MOF-199" and "[Cu₃(BTC)₂]ₙ" are the same MOF
+    ii) Ex: "[Zr₆O₄(OH)₄(BDC)₆]" and "UiO-66" are the same MOF
+    iii) Sometimes, the author will refer to a MOF named earlier by "Compound 1" or "Compound 2" etc. Ex: The text "The MOF [Cu₃(BTC)₂] 1) was synthesized" implies that "[Cu₃(BTC)₂]" and "Compound 1" are the same MOF
+    iv) Do NOT assign a "has_alias" relationship to two "MOFs" just because they both have the word "Compound" or "Complex" in them. Ex: "Complex 1" and "Complex 2" should never have a "has_alias" relationship.
+
+3) Note that not all chemical formulas mentioned in the text are MOFs. Some may be precursors used in the synthesis of the MOF. Use your expert chemistry knowledge to correctly identify when a chemical formula is a MOF. Here are some things to look for in the formula to tell if its a MOF:
+    i) Metal ions, typically transition metals like Zinc (Zn), Copper (Cu), Chromium (Cr), Zirconium (Zr), Cobalt (Co), etc.
+    ii) Organic molecules, often carboxylates or imidazolates, that act as linkers between metal ions. Sometimes abbrieviated - Ex: Benzene-1,3,5-tricarboxylate (BTC), Fumarate (FUM), 1,4-Benzenediphosphonate (BDP)
+
+
+
+
+Below are a number of examples of text and their extracted entities and relationships.
+
+Example 1:
+- "Herein, we develop a facile carbon coating strategy to prepare CuOx@C with carbon skin through one-pot pyrolysis of a Cu-based metal organic framework HKUST-1 (Cu3(BTC)2, Cu-BTC)."
+
+    -From this text, "Cu3(BTC)2, Cu-BTC" and "HKUST-1" should both be identified as "MOF" node types. Since these two entities are referring to the same MOF under two different naming conventions,  
+    HKUST-1 should have a "has_alias" relationship with Cu3(BTC)2, Cu-BTC. 
+    
+    `{{"head": "Cu3(BTC)2, Cu-BTC"", "head_type": "MOF", "relation": "has_alias", "tail": "HKUST-1", "tail_type": "MOF"}}`
+'''
+
+system_prompt = (
+    "# Knowledge Graph Instructions for GPT\n"
+    "## 1. Overview\n"
+    "You are a top-tier algorithm with expert chemistry knowledge designed for extracting information in structured "
+    "formats to build a knowledge graph.\n"
+    "Your particular field of expertise is in Metal-Organic Frameworks (MOFs)"
+    "and you have expert ability in reading and understanding scientific literature related to MOFs"
+    "Try to capture as much information from the text as possible without "
+    "sacrificing accuracy. Do not add any information that is not explicitly "
+    "mentioned in the text.\n"
+    "- **Nodes** represent entities and concepts.\n"
+    "- The aim is to achieve simplicity and clarity in the knowledge graph, making it\n"
+    "accessible for a vast audience.\n"
+    "## 2. Labeling Nodes\n"
+    "- **Consistency**: Ensure you use available types for node labels.\n"
+    "Ensure you use basic or elementary types for node labels.\n"
+    "- For example, when you identify an entity representing a person, "
+    "always label it as **'person'**. Avoid using more specific terms "
+    "like 'mathematician' or 'scientist'."
+    "- **Node IDs**: Never utilize integers as node IDs. Node IDs should be "
+    "names or human-readable identifiers found in the text.\n"
+    "- **Relationships** represent connections between entities or concepts.\n"
+    "Ensure consistency and generality in relationship types when constructing "
+    "knowledge graphs. Instead of using specific and momentary types "
+    "such as 'BECAME_PROFESSOR', use more general and timeless relationship types "
+    "like 'PROFESSOR'. Make sure to use general and timeless relationship types!\n"
+    "## 3. Coreference Resolution\n"
+    "- **Maintain Entity Consistency**: When extracting entities, it's vital to "
+    "ensure consistency.\n"
+    'If an entity, such as "John Doe", is mentioned multiple times in the text '
+    'but is referred to by different names or pronouns (e.g., "Joe", "he"),'
+    "always use the most complete identifier for that entity throughout the "
+    'knowledge graph. In this example, use "John Doe" as the entity ID.\n'
+    "Remember, the knowledge graph should be coherent and easily understandable, "
+    "so maintaining consistency in entity references is crucial.\n"
+    "## 4. Strict Compliance\n"
+    "Adhere to the rules strictly. Non-compliance will result in termination."
+    )
+
+human_string_parts = ( 
+        "As a chemist, I am looking for a specific MOF found within this text"
+        "Here is information about the MOF I am looking for:\n"
+        "{csd_info}\n"
+        f"{context}\n"  
+        "For the following text, find the MOF I am looking for and all its aliases"
+        "\n\n Text: {input}"
+    )
+
+
+default_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            system_prompt,
+            
+        ),
+        (
+            "human",
+            human_string_parts,
+        ),
+    ]
+)
--- metadata.py
+++ metadata.py
@@ -1,7 +1,22 @@
 import pandas as pd
+from langchain_community.graphs import Neo4jGraph
+from langchain_openai import ChatOpenAI
+from langchain_experimental.graph_transformers import LLMGraphTransformer
 from Neo4J_KG import clean_text, process_file, extract_nodes
+from MOFKG_Properties import ALLOWED_NODES, ALLOWED_RELATIONSHIPS, NODE_PROPERTIES, RELATIONSHIP_PROPERTIES
+from Prompt_MOF import default_prompt
+import os
 
-data = pd.read_csv("cif data/csv/CoRE_or_QMOF_properties.csv")
+
+# Make sure you download APOC and GDS plugins if using local host
+# Make sure you have opened the graph first on Neo4j Local
+os.environ["NEO4J_URI"] = "bolt://localhost:7687"
+os.environ["NEO4J_USERNAME"] = "neo4j"
+os.environ["NEO4J_PASSWORD"] = "AI4ChemS"
+
+graph = Neo4jGraph(refresh_schema=False)
+
+data = pd.read_csv("cif data/csv/CoRE_QMOF_expanded_filtered.csv")
 
 
 input_file = "data/md/1/1.md"
@@ -23,22 +38,52 @@ metadata = {
     "MOFs": {}
 }
 
+RefCodes = []
+
 # Filter DataFrame by DOI
 filtered_df = data[data["DOI"] == input_doi]
 
 # Iterate through the filtered DataFrame and populate the MOFs in metadata
 for _, row in filtered_df.iterrows():
     mof_name = row["CSD code"]
-    band_gap = row["Band gap (eV)"]
+    metals = row["Metal types"]
+    space_group = row["Space group"]
     
     # Store the MOF and its band gap in the metadata
     metadata["MOFs"][mof_name] = {
-        "Band Gap": band_gap
+        "Metals": metals,
+        "Space Group": space_group
     }
+
+RefCodes = list(metadata['MOFs'].keys())
+
+csd_info = f"CSD Reference Code: {metadata['MOFs'][RefCodes[0]]}, \
+Properties: [Metal: {metadata['MOFs'][RefCodes[0]]['Metals']}, \
+Space Group: {metadata['MOFs'][RefCodes[0]]['Space Group']}]"
 
 
 #Pass text and meta data to the proces
-#docs = process_file(text, metadatas=metadata)
+docs = process_file(text, metadatas={'id': metadata['id']})
+#print(docs)
 
-print(metadata)
 
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+llm_transformer = LLMGraphTransformer(llm=llm,
+                                        allowed_nodes=ALLOWED_NODES,
+                                        allowed_relationships=ALLOWED_RELATIONSHIPS,
+                                        node_properties=NODE_PROPERTIES, 
+                                        relationship_properties=RELATIONSHIP_PROPERTIES,
+                                        prompt = default_prompt.partial(csd_info = csd_info))
+
+'''
+prompt_message = default_prompt.partial(csd_info = csd_info).format_messages(input="The quick brown fox jumps over the lazy dog.")
+
+# This will output a list of HumanMessage and SystemMessage objects ready for use
+for message in prompt_message:
+    if 1:
+        print(f"Human: {message.content}")
+    else:
+        print(f"System: {message.content}")'''
+
+graph_docs = extract_nodes(docs, llm_transformer, 10)
+graph.add_graph_documents(graph_docs, baseEntityLabel=True, include_source=True)
