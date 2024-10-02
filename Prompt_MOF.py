5049f6634e7fcfa4988f6d97a483ef0855edaabd
iamthomaspruyn <tompruyn84@gmail.com>
2024-10-02 15:55:13 -0400

    prompt mof detects metals, metadata has loop for all mofs now

--- Prompt_MOF.py
+++ Prompt_MOF.py
@@ -92,9 +92,15 @@ Here is some context to help you perform this task, and identify the nodes and r
 
 ### **Node Types:**
 - **MOF (Metal-Organic Framework):** Refers to compounds consisting of metal ions or clusters coordinated to organic ligands. MOFs are sometimes reffered to as "Coordination Polymers". No standard naming convention exists, and they may or may not be presented as a chemical formula. Only create nodes for specific MOFs named in the paper! If the paper mentions "Zinc MOFs", do not extract this as a node. But if the paper said "[Zn₄O(BDC)₃]", do extract this as a node.
+- **Metal:** A metal that is part the MOF structure, this may or may not be indicated in the MOF name. Only identify metals if they are contained within a MOF. Other references to metals within a paper can be ignored.
+- **Linker:** An organic molecule connecting metal ions or clusters in a MOF. This may or may not be indicated in the MOF name.
+- **Synthesis:** The text may include information regarding how a MOF was synthesized. If this information exists in the text, create a "Synthesis" node type and store the synthesis procedure in the node "description". If the paper does not include synthesis information, do not create a node.
 
 ### **Relationship Types:**
 - **Has_Alias:** Indicates that two "MOF" type nodes refer to the same MOF, just under a different name. Ex: "MOF-801" may be reffered to as "Complex 1" in a different part of the text. 
+- **Has_Metal:** Indicates that a "MOF" contains a specific "Metal". One MOF can have multiple metals
+- **Has_Linker:** Indicates that a "MOF" contains a specific "Linker". One MOF can have multiple linkers.
+- **Has_Synthesis:** Indicates that the text contains synthesis information about a specific "MOF" node.
 
 ### **Important Guidelines:**
 1) No standard naming convention exists for MOFs. It is your job to correctly identify MOF node types in the given text. Here are some examples of how MOFs can be named:
@@ -103,10 +109,11 @@ Here is some context to help you perform this task, and identify the nodes and r
     iii) Based on chemical formulas - Ex: "[Zr₆O₄(OH)₄(BDC)₆]"
 
 2) It is your job to identify when the author of the text referring to a previously mentioned MOF under different naming convention. Give a "has_alias" relationship to these MOFs if you believe with certaintity that this is the case.
-    i) Ex: "HKUST-1", "MOF-199" and "[Cu₃(BTC)₂]ₙ" are the same MOF
-    ii) Ex: "[Zr₆O₄(OH)₄(BDC)₆]" and "UiO-66" are the same MOF
+    i) Ex: "HKUST-1", "MOF-199" and "[Cu₃(BTC)₂]ₙ" are the same MOF. Understand the text to make this connection.
+    ii) Ex: "[Zr₆O₄(OH)₄(BDC)₆]" and "UiO-66" are the same MOFUnderstand the text to make this connection.
     iii) Sometimes, the author will refer to a MOF named earlier by "Compound 1" or "Compound 2" etc. Ex: The text "The MOF [Cu₃(BTC)₂] 1) was synthesized" implies that "[Cu₃(BTC)₂]" and "Compound 1" are the same MOF
-    iv) Do NOT assign a "has_alias" relationship to two "MOFs" just because they both have the word "Compound" or "Complex" in them. Ex: "Complex 1" and "Complex 2" should never have a "has_alias" relationship.
+    iv) Do NOT assign a "has_alias" relationship to two "MOFs" just because they both have the word "Compound" or "Complex" in them. Ex: "Complex 1" and "Complex 2" should never have a "has_alias" relationship. "Compound 1" and "Compound 2" should never have a "has_alias" relationship.
+    v) NEVER assign a "has_alias" relationship for just one node. Make sure it is two unique nodes
 
 3) Note that not all chemical formulas mentioned in the text are MOFs. Some may be precursors used in the synthesis of the MOF. Use your expert chemistry knowledge to correctly identify when a chemical formula is a MOF. Here are some things to look for in the formula to tell if its a MOF:
     i) Metal ions, typically transition metals like Zinc (Zn), Copper (Cu), Chromium (Cr), Zirconium (Zr), Cobalt (Co), etc.
@@ -119,10 +126,13 @@ Below are a number of examples of text and their extracted entities and relation
 
 Example 1:
 - "Herein, we develop a facile carbon coating strategy to prepare CuOx@C with carbon skin through one-pot pyrolysis of a Cu-based metal organic framework HKUST-1 (Cu3(BTC)2, Cu-BTC)."
-
     -From this text, "Cu3(BTC)2, Cu-BTC" and "HKUST-1" should both be identified as "MOF" node types. Since these two entities are referring to the same MOF under two different naming conventions,  
-    HKUST-1 should have a "has_alias" relationship with Cu3(BTC)2, Cu-BTC. 
+    HKUST-1 should have a "has_alias" relationship with Cu3(BTC)2, Cu-BTC. As well, "Cu" should be identified as a metal node type, and should have a "has_metal" relationship with 
+    Cu3(BTC)2, Cu-BTC. BTC should be identified as a "organic linker" node type, and should have a "has_organic_linker" relationship with Cu3(BTC)2, Cu-BTC. Also note that CuOx@C is a MOF-derived material, 
+    not a MOF. Therefore it should be not identified as a MOF or any other type of node. The correct classification of this text is:
     
+    `{{"head": "Cu3(BTC)2, Cu-BTC"", "head_type": "MOF", "relation": "has_metal", "tail": "Cu", "tail_type": "Metal"}}`
+    `{{"head": "Cu3(BTC)2, Cu-BTC"", "head_type": "MOF", "relation": "has_linker", "tail": "BTC", "tail_type": "Linker"}}`
     `{{"head": "Cu3(BTC)2, Cu-BTC"", "head_type": "MOF", "relation": "has_alias", "tail": "HKUST-1", "tail_type": "MOF"}}`
 '''
 
@@ -166,11 +176,13 @@ system_prompt = (
     )
 
 human_string_parts = ( 
-        "As a chemist, I am looking for a specific MOF found within this text"
+        "Your job is to find only ONE specific MOF in this text, and add it to the knowledge graph\n"
+        "I am going to present you with some information that can help. The MOF you find should have the same Metals, Chemical Formula, and Space Group"
+        "Multiple MOFs may share the same Space Group or Meta, but only create a MOF node type if you are certain it is the same MOF I am looking for."
         "Here is information about the MOF I am looking for:\n"
         "{csd_info}\n"
         f"{context}\n"  
-        "For the following text, find the MOF I am looking for and all its aliases"
+        "Use your expert chemistry knowledge to create a MOF node type for the MOF I am looking for, as well as all its aliases."
         "\n\n Text: {input}"
     )
 
--- metadata.py
+++ metadata.py
@@ -7,7 +7,6 @@ from MOFKG_Properties import ALLOWED_NODES, ALLOWED_RELATIONSHIPS, NODE_PROPERTI
 from Prompt_MOF import default_prompt
 import os
 
-
 # Make sure you download APOC and GDS plugins if using local host
 # Make sure you have opened the graph first on Neo4j Local
 os.environ["NEO4J_URI"] = "bolt://localhost:7687"
@@ -18,9 +17,8 @@ graph = Neo4jGraph(refresh_schema=False)
 
 data = pd.read_csv("cif data/csv/CoRE_QMOF_expanded_filtered.csv")
 
-
-input_file = "data/md/1/1.md"
-input_doi = "10.1039/c3ce40346g" #when we scale this - this can be easily found by appending a new column in our .csv with a "document id- that way DOI for each paper is easily found in the same row
+input_file = "data/md/4/4.md"
+input_doi = "10.1039/C5RA15937G"  # This can be easily found by appending a new column in the .csv with a "document id"
 
 # Read md
 with open(input_file, "r", encoding="utf-8") as f:
@@ -29,16 +27,11 @@ with open(input_file, "r", encoding="utf-8") as f:
 # Clean the text
 text = clean_text(content)
 
-
-# Find relevant metadatas in dataframe
-
 # Initialize metadata dictionary
 metadata = {
     "id": input_doi,
     "MOFs": {}
 }
-
-RefCodes = []
 
 # Filter DataFrame by DOI
 filtered_df = data[data["DOI"] == input_doi]
@@ -48,42 +41,39 @@ for _, row in filtered_df.iterrows():
     mof_name = row["CSD code"]
     metals = row["Metal types"]
     space_group = row["Space group"]
+    molecular_formula = row["Molecular formula"]
     
     # Store the MOF and its band gap in the metadata
     metadata["MOFs"][mof_name] = {
         "Metals": metals,
-        "Space Group": space_group
+        "Space Group": space_group,
+        "Molecular formula": molecular_formula
     }
 
 RefCodes = list(metadata['MOFs'].keys())
 
-csd_info = f"CSD Reference Code: {metadata['MOFs'][RefCodes[0]]}, \
-Properties: [Metal: {metadata['MOFs'][RefCodes[0]]['Metals']}, \
-Space Group: {metadata['MOFs'][RefCodes[0]]['Space Group']}]"
-
-
-#Pass text and meta data to the proces
-docs = process_file(text, metadatas={'id': metadata['id']})
-#print(docs)
-
-
 llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
-llm_transformer = LLMGraphTransformer(llm=llm,
-                                        allowed_nodes=ALLOWED_NODES,
-                                        allowed_relationships=ALLOWED_RELATIONSHIPS,
-                                        node_properties=NODE_PROPERTIES, 
-                                        relationship_properties=RELATIONSHIP_PROPERTIES,
-                                        prompt = default_prompt.partial(csd_info = csd_info))
 
-'''
-prompt_message = default_prompt.partial(csd_info = csd_info).format_messages(input="The quick brown fox jumps over the lazy dog.")
+# Loop through each RefCode and update csd_info
+for ref_code in RefCodes:
+    csd_info = f"Properties: [Metals: {metadata['MOFs'][ref_code]['Metals']}, \
+    Space Group: {metadata['MOFs'][ref_code]['Space Group']}, \
+    Molecular Formula: {metadata['MOFs'][ref_code]['Molecular formula']}]"
 
-# This will output a list of HumanMessage and SystemMessage objects ready for use
-for message in prompt_message:
-    if 1:
-        print(f"Human: {message.content}")
-    else:
-        print(f"System: {message.content}")'''
+    # Print to check the csd_info for each iteration
+    print(csd_info)
 
-graph_docs = extract_nodes(docs, llm_transformer, 10)
-graph.add_graph_documents(graph_docs, baseEntityLabel=True, include_source=True)
+    # Pass text and metadata to the process_file function
+    docs = process_file(text, metadatas={'id': metadata['id']})
+
+    # Create a new transformer for each RefCode with updated csd_info
+    llm_transformer = LLMGraphTransformer(llm=llm,
+                                          allowed_nodes=ALLOWED_NODES,
+                                          allowed_relationships=ALLOWED_RELATIONSHIPS,
+                                          node_properties=NODE_PROPERTIES, 
+                                          relationship_properties=RELATIONSHIP_PROPERTIES,
+                                          prompt=default_prompt.partial(csd_info=csd_info))
+
+    # Extract nodes and add to the graph
+    graph_docs = extract_nodes(docs, llm_transformer, 10)
+    graph.add_graph_documents(graph_docs, baseEntityLabel=True, include_source=True)
